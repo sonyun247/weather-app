9b8ad844505615b692f3f174cbb6fd59a8854773
LEE Seungje <38553239+sonyun247@users.noreply.github.com>
2020-10-07 17:55:46 +0900

    get location & weather

---
+++ Loading.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>주변 둘러보는 중...</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#FDF6AA",
+  },
+  text: {
+    color: "#2c2c2c",
+    fontSize: 30,
+  },
+});
+
+export default Loading;
